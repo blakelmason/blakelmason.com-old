a4d627da0637e4c382ea219bea6419759abc2143
Blake Mason <blake@Blakes-MBP.fios-router.home>
2018-07-12 23:05:06 -0700

    fixed express route

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 
-app.use("/", express.static(__dirname + "/build"));
+app.use("/*", express.static(__dirname + "/build"));
 
 
 app.listen(PORT, function () {
