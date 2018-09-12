8165d4cea06af3ab9d3237504b707ee800467e24
Blake Mason <blake@Blakes-MBP.fios-router.home>
2018-09-11 23:53:05 -0700

    NYT app

--- client/src/pages/portfolio/Portfolio.js
+++ client/src/pages/portfolio/Portfolio.js
@@ -9,6 +9,7 @@ import trivia from '../../images/trivia.gif';
 import pokemon from '../../images/pokemon.gif';
 import burger from '../../images/burger.gif';
 import lotr from '../../images/lotr.gif';
+import city from '../../images/city.gif';
 import fill from '../../images/fill.jpg';
 
 export default class Portfolio extends Component {
@@ -20,9 +21,10 @@ export default class Portfolio extends Component {
             <h3 className="mt-3 mb-0 font-weight-bold">Portfolio</h3>
             <hr className="mt-3 mb-4" />
             <div className="row text-center">
+              <PortfolioCard image={city} href={'https://ancient-shore-12375.herokuapp.com/'} text="NYT React App" />
               <PortfolioCard image={lotr} href={'https://blakelmason.github.io/react-clicky-game/'} text="Clicky Game" />
-              <PortfolioCard image={hangman} href={'https://blakelmason.github.io/projects/hangman-adventure/index.html'} text="Hangman Adventure!" />
               <PortfolioCard image={burger} href={'https://morning-scrubland-74719.herokuapp.com/'} text="Eat-Da-Burger" />
+              <PortfolioCard image={hangman} href={'https://blakelmason.github.io/projects/hangman-adventure/index.html'} text="Hangman Adventure!" />
               <PortfolioCard image={dnd} href={'https://aaronsy1.github.io/firstGroupProject/spells.html'} text="D&D Tool" />
               <PortfolioCard image={darkSouls} href={'https://blakelmason.github.io/projects/giphy-tool/index.html'} text="Giphy Tool" />
               <PortfolioCard image={trivia} href={'https://blakelmason.github.io/projects/trivia-game/index.html'} text="Trivia Game" />
